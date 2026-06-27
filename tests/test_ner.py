ed60bbd925f26ae9ede86749384a087cdecc4db0
Balaram <gummadibu@gmail.com>
2026-06-27 22:42:47 +0530

    Delete tests directory

--- tests/test_ner.py
+++
@@ -1,8 +0,0 @@
-from src.ner_extract import extract_entities
-
-def test_ner():
-    text = "Invoice from Amazon on 13/08/2023 with total ₹6700"
-    ents = extract_entities(text)
-    assert "₹6700" in ents.get("MONEY", [""])
-    assert "13/08/2023" in ents.get("DATE", [""])
-    print("✔ NER test passed:", ents)
