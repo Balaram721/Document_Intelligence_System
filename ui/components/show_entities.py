119af22e62872e73f0c66d7f2d0ddcffaccf8647
Balaram <gummadibu@gmail.com>
2026-06-27 22:43:04 +0530

    Delete ui directory

--- ui/components/show_entities.py
+++
@@ -1,5 +0,0 @@
-import streamlit as st
-
-def render_entities(entities):
-    st.subheader("🔎 Named Entity Extraction")
-    st.json(entities)
--- ui/components/show_summary.py
+++
@@ -1,5 +0,0 @@
-import streamlit as st
-
-def render_summary(summary):
-    st.subheader("📝 Document Summary")
-    st.write(summary)
